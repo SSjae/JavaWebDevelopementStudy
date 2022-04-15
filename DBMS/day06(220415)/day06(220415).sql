ba9b387439d2e06cd6c0e1176452e70cfe169bf7
SS__jae2 <75529454+SSjae@users.noreply.github.com>
2022-04-15 23:27:53 +0900

    [DBMS] ums 예제 JDBC로 변환, 팀플 조

---
+++ DBMS/day06(220415)/day06(220415).sql
@@ -0,0 +1,26 @@
+use web0315;
+drop table product;
+drop table user;
+
+create table user (
+	useridx int primary key auto_increment,
+	userid varchar(300) unique,
+    userpw varchar(300) not null,
+    username varchar(300) not null,
+    userage int,
+    userphone varchar(300),
+    useraddr varchar(1000)
+);
+create table product (
+	prodnum int primary key auto_increment,
+    prodname varchar(1000),
+    prodprice int,
+    prodamount int,
+    prodinfo varchar(4000),
+    likecnt int default 0,
+    userid varchar(300),
+    constraint user_product_fk foreign key(userid) references user(userid)
+);
+
+select * from user;
+select * from product;
